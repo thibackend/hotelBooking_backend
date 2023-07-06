a555f5a8f553a70efbfd9a1444f3ce9de3b2bcc6
ThangNH23 <huuthang533540403@gmail.com>
2023-07-06 17:01:26 +0700

    update feature comment

--- app/Http/Controllers/hotel_booking_api/ReviewRoomController.php
+++ app/Http/Controllers/hotel_booking_api/ReviewRoomController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\hotel_booking_api;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class ReviewRoomController extends Controller
 
     public function index(Request $request)
     {
-        
+        return response()->json(comments::all());
     }
 
     /**
@@ -29,20 +30,18 @@ class ReviewRoomController extends Controller
      */
     public function store(Request $request)
     {
-        
-        $user_id=$request->user_id;
-        $room_id=$request->room_id;
-        $content=$request->content;
-        $user=Users::where("id",$user_id)->first();
-        if($user){
-            $comment=new comments();
-            $comment->user_id=$user->id;
-            $comment->room_id=$room_id;
-            $comment->content=$content;
+        $user_id = $request->user_id;
+        $room_id = $request->room_id;
+        $content = $request->content;
+        $user = Users::where("id", $user_id)->first();
+        if ($user) {
+            $comment = new comments();
+            $comment->user_id = $user->id;
+            $comment->room_id = $room_id;
+            $comment->content = $content;
             $comment->save();
-            return response()->json([
-                "status" => 200
-            ]);
+            $data = $this->show($request->room_id);
+            return response()->json($data);
         }
         return response()->json([
             "status" => 500
@@ -52,14 +51,14 @@ class ReviewRoomController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public function show(string $id)
     {
-        $user_comment=comments::where("room_id",$id)
-        ->join("users","comments.user_id","=","users.id")
-        ->join("rooms","comments.room_id","=","rooms.id")
-        ->select("users.id","users.name","users.image","comments.content", "comments.created_at")
-        ->groupBy("users.id", "users.name", "users.image", "comments.content","comments.created_at")
-        ->get();
+        $user_comment = comments::where("room_id", $id)
+            ->join("users", "comments.user_id", "=", "users.id")
+            ->join("rooms", "comments.room_id", "=", "rooms.id")
+            ->select("users.id", "users.name", "users.image", "comments.content", "comments.created_at")
+            ->groupBy("users.id", "users.name", "users.image", "comments.content", "comments.created_at")
+            ->get();
         return response()->json($user_comment);
     }
 
